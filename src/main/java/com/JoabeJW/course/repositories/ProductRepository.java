cbc9f17fe2d1a968bb9bb26f4bd23b3a6135d4ca
joabe <joabe.18@hotmail.com>
2023-01-05 11:42:49 -0300

    Product

---
+++ src/main/java/com/JoabeJW/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.JoabeJW.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.JoabeJW.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+
+}
