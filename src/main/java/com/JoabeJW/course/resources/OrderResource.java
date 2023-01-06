65f02e4ac1da52060af513e05b8cd4c8387ff0c5
joabe <joabe.18@hotmail.com>
2023-01-06 15:20:10 -0300

    User insert

--- src/main/java/com/JoabeJW/course/resources/OrderResource.java
+++ src/main/java/com/JoabeJW/course/resources/OrderResource.java
@@ -9,28 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.JoabeJW.course.entities.User;
-import com.JoabeJW.course.services.UserService;
+import com.JoabeJW.course.entities.Order;
+import com.JoabeJW.course.services.OrderService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/orders")
 public class OrderResource {
-	
+
 	@Autowired
-	private UserService service;
-	
+	private OrderService service;
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
-		List<User> list = service.findAll();
+	public ResponseEntity<List<Order>> findAll() {
+		List<Order> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
-		User obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
-		
-	}
-	
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		Order obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
