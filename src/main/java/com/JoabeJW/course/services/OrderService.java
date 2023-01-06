457686cb69a2ba642ce7847c6abcd23ce50cafbc
joabe <joabe.18@hotmail.com>
2023-01-06 15:27:29 -0300

    User delete

--- src/main/java/com/JoabeJW/course/services/OrderService.java
+++ src/main/java/com/JoabeJW/course/services/OrderService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.JoabeJW.course.entities.Order;
-import com.JoabeJW.course.entities.User;
 import com.JoabeJW.course.repositories.OrderRepository;
 
 @Service
